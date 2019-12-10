69056abeef42e82270bc03ef620d592ee1380849
Will Rowston <willrowston@gmail.com>
2019-12-10 11:41:59 -0500

    added joke controller

---
+++ src/main/java/guru/springframework/chucknorrisjokesapp/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package guru.springframework.chucknorrisjokesapp.controllers;
+
+import guru.springframework.chucknorrisjokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", this.jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
