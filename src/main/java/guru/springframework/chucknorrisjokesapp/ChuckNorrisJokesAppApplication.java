2bc66a54c8f78533ca968c7026e5a4fa10a57c35
Will Rowston <willrowston@gmail.com>
2019-12-10 11:15:35 -0500

    first commit

---
+++ src/main/java/guru/springframework/chucknorrisjokesapp/ChuckNorrisJokesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.chucknorrisjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/chucknorrisjokesapp/ChuckNorrisJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.chucknorrisjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
